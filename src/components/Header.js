3b26667cd11228925d29234b759560471403aca6
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-07-31 13:24:02 -0500

    add my header component and styled the header.css

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <header className='header-style'>
+        <div className="my-name">
+            <h3>Fabrice Ngalemo</h3>
+            <h4>Software Engineer</h4>
+        </div>
+        <div className="socila-icon">
+            myicons here 
+        </div>
+        <div className="my-resume">
+            <h3>RESUME</h3>
+        </div>
+    </header>
+  )
+}
+
+export default Header

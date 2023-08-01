3ba08ce6dfbef2ab41c1f82462a046032df5fe6c
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-08-01 13:20:43 -0500

    add the harm menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 import './NavBar.css'
 
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav>
         <Link>
             <img src="" alt="Logo" className="logo" />
         </Link>
-        <div className="h-menu">
+        <div className="h-menu" onClick={()=>{
+            setMenuOpen(!menuOpen);
+        }}>
         <span></span>
             <span></span>
             <span></span>
         </div>
-        <ul>
+        <ul className={menuOpen ? 'open': ''}>
             <li>
                 <NavLink to="/">ABOUT ME</NavLink>
 
